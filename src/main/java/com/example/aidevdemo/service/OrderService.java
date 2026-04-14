4e76165082c957260c5439c2149f6a3795d88478
dgc-git <18280349792@163.com>
2026-04-14 16:30:39 +0800

    hello

--- src/main/java/com/example/aidevdemo/service/OrderService.java
+++ src/main/java/com/example/aidevdemo/service/OrderService.java
@@ -39,6 +39,8 @@ public class OrderService {
         System.out.println("测试没有的情况");
         System.out.println("hello");
         System.out.println("我再测试下");
+        System.out.println("我再测试下");
+        System.out.println("我再测试下");
         System.out.println("处理订单: " + orderId);
     }
 }
