e56dd48e731f647c80c071e33ab68112aaeeecff
dgc-git <18280349792@163.com>
2026-04-14 12:01:24 +0800

    chore: initial projject setup

---
+++ src/main/java/com/dgc/aidevdemo/AiDevDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dgc.aidevdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDevDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDevDemoApplication.class, args);
+    }
+
+}
